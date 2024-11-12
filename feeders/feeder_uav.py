742564392ec1bbd8d3f0ea3b1986925ad343eb50
RoseLYi <161707875+RoseLYi@users.noreply.github.com>
2024-11-12 19:46:38 +0800

    Update feeder_uav.py

--- feeders/feeder_uav.py
+++ feeders/feeder_uav.py
@@ -1,110 +1,105 @@
 import numpy as np
-
 from torch.utils.data import Dataset
-
 from feeders import tools
 
 
-class Feeder(Dataset):
-    def __init__(self, data_path, label_path=None, p_interval=1, split='train', random_choose=False, random_shift=False,
-                 random_move=False, random_rot=False, window_size=-1, normalization=False, debug=False, use_mmap=False,
-                 bone=False, vel=False):
+class SkeletonDataset(Dataset):
+    def __init__(self, data_path, label_path=None, interval=1, mode='train', random_select=False, random_shift=False,
+                 random_move=False, apply_rotation=False, seq_length=-1, normalize=False, debug=False, mmap_mode=False,
+                 use_bone=False, apply_velocity=False):
         """
-        :param data_path:
-        :param label_path:
-        :param split: training set or test set
-        :param random_choose: If true, randomly choose a portion of the input sequence
-        :param random_shift: If true, randomly pad zeros at the begining or end of sequence
-        :param random_move:
-        :param random_rot: rotate skeleton around xyz axis
-        :param window_size: The length of the output sequence
-        :param normalization: If true, normalize input sequence
-        :param debug: If true, only use the first 100 samples
-        :param use_mmap: If true, use mmap mode to load data, which can save the running memory
-        :param bone: use bone modality or not
-        :param vel: use motion modality or not
-        :param only_label: only load label for ensemble score compute
+        :param data_path: Path to the data file
+        :param label_path: Path to the label file
+        :param mode: Mode for dataset - either 'train' or 'test'
+        :param random_select: Randomly select a portion of the sequence
+        :param random_shift: Randomly shift the sequence with padding
+        :param random_move: Apply random movements to the sequence
+        :param apply_rotation: Rotate skeleton around xyz axis
+        :param seq_length: Target length for the sequence
+        :param normalize: Apply normalization
+        :param debug: Enable debug mode for limited samples
+        :param mmap_mode: Use memory mapping for data loading
+        :param use_bone: Enable bone modality
+        :param apply_velocity: Enable velocity modality
         """
-
         self.debug = debug
         self.data_path = data_path
         self.label_path = label_path
-        self.split = split
-        self.random_choose = random_choose
+        self.mode = mode
+        self.random_select = random_select
         self.random_shift = random_shift
         self.random_move = random_move
-        self.window_size = window_size
-        self.normalization = normalization
-        self.use_mmap = use_mmap
-        self.p_interval = p_interval
-        self.random_rot = random_rot
-        self.bone = bone
-        self.vel = vel
-        # self.load_data()
+        self.seq_length = seq_length
+        self.normalize = normalize
+        self.mmap_mode = mmap_mode
+        self.interval = interval
+        self.apply_rotation = apply_rotation
+        self.use_bone = use_bone
+        self.apply_velocity = apply_velocity
+        # Load data during initialization
+        self._load_data()
 
-    def load_data(self):
-        # data: N C V T M
-        npz_data = np.load(self.data_path)
-        if self.split == 'train':
-            self.data = npz_data['x_train']
-            self.label = npz_data['y_train']#np.where(npz_data['y_train'] > 0)[1]
-            self.sample_name = ['train_' + str(i) for i in range(len(self.data))]
-        elif self.split == 'test':
-            self.data = npz_data['x_test']
-            self.label = npz_data['y_test']#np.where(npz_data['y_test'] > 0)[1]
-            self.sample_name = ['test_' + str(i) for i in range(len(self.data))]
+    def _load_data(self):
+        # Load data based on mode
+        data_content = np.load(self.data_path)
+        if self.mode == 'train':
+            self.data = data_content['x_train']
+            self.labels = data_content['y_train']
+            self.sample_names = [f'train_{i}' for i in range(len(self.data))]
+        elif self.mode == 'test':
+            self.data = data_content['x_test']
+            self.labels = data_content['y_test']
+            self.sample_names = [f'test_{i}' for i in range(len(self.data))]
         else:
-            raise NotImplementedError('data split only supports train/test')
-        # N, T, _ = self.data.shape
-        # self.data = self.data.reshape((N, T, 2, 17, 3)).transpose(0, 4, 1, 3, 2)
+            raise ValueError('Mode should be either "train" or "test"')
+
         self.data = self.data.transpose(0, 4, 1, 3, 2)
 
-    def get_mean_map(self):
-        data = self.data
-        N, C, T, V, M = data.shape
-        self.mean_map = data.mean(axis=2, keepdims=True).mean(axis=4, keepdims=True).mean(axis=0)
-        self.std_map = data.transpose((0, 2, 4, 1, 3)).reshape((N * T * M, C * V)).std(axis=0).reshape((C, 1, V, 1))
+    def _compute_mean_std(self):
+        data_shape = self.data.shape
+        self.mean_map = self.data.mean(axis=(2, 4), keepdims=True).mean(axis=0)
+        self.std_map = self.data.transpose((0, 2, 4, 1, 3)).reshape((-1, data_shape[1] * data_shape[3])).std(axis=0).reshape((data_shape[1], 1, data_shape[3], 1))
 
     def __len__(self):
-        return len(self.label)
+        return len(self.labels)
 
-    def __iter__(self):
-        return self
+    def __getitem__(self, idx):
+        sample_data = self.data[idx]
+        label = self.labels[idx]
+        sample_data = np.array(sample_data)
 
-    def __getitem__(self, index):
-        C, T, V, M = self.data[1].shape  #获取到每个样本数据的shape
-        data_numpy = self.data[index]    #获取到一个样本
-        label = self.label[index]        #获取到对应的label
-        data_numpy = np.array(data_numpy)        #将数据转换为numpy兼容格式
-        if not(np.any(data_numpy)):
-            data_numpy = np.array(self.data[0])
-            
-        valid_frame_num = np.sum(data_numpy.sum(0).sum(-1).sum(-1) != 0) #和下一个函数冲突，全0会被报错
-        # reshape Tx(MVC) to CTVM
-        data_numpy = tools.valid_crop_resize(data_numpy, valid_frame_num, self.p_interval, self.window_size)
-        if self.random_rot:
-            data_numpy = tools.random_rot(data_numpy)
-        if self.bone:
+        if not sample_data.any():
+            sample_data = np.array(self.data[0])
+
+        valid_frames = np.sum(sample_data.sum(0).sum(-1).sum(-1) != 0)
+        sample_data = tools.valid_crop_resize(sample_data, valid_frames, self.interval, self.seq_length)
+
+        if self.apply_rotation:
+            sample_data = tools.random_rot(sample_data)
+
+        if self.use_bone:
             from .bone_pairs import ntu_pairs
-            bone_data_numpy = np.zeros_like(data_numpy)
-            for v1, v2 in ntu_pairs:
-                bone_data_numpy[:, :, v1 - 1] = data_numpy[:, :, v1 - 1] - data_numpy[:, :, v2 - 1]
-            data_numpy = bone_data_numpy
-        if self.vel:
-            data_numpy[:, :-1] = data_numpy[:, 1:] - data_numpy[:, :-1]
-            data_numpy[:, -1] = 0
+            bone_data = np.zeros_like(sample_data)
+            for start, end in ntu_pairs:
+                bone_data[:, :, start - 1] = sample_data[:, :, start - 1] - sample_data[:, :, end - 1]
+            sample_data = bone_data
 
-        return data_numpy, label, index
+        if self.apply_velocity:
+            sample_data[:, :-1] = sample_data[:, 1:] - sample_data[:, :-1]
+            sample_data[:, -1] = 0
 
-    def top_k(self, score, top_k):
-        rank = score.argsort()
-        hit_top_k = [l in rank[i, -top_k:] for i, l in enumerate(self.label)]
-        return sum(hit_top_k) * 1.0 / len(hit_top_k)
+        return sample_data, label, idx
+
+    def top_k_accuracy(self, scores, top_k):
+        rankings = scores.argsort()
+        hit_top_k = [lbl in rankings[i, -top_k:] for i, lbl in enumerate(self.labels)]
+        return sum(hit_top_k) / len(hit_top_k)
 
 
-def import_class(name):
-    components = name.split('.')
-    mod = __import__(components[0])
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-    return mod
+def load_class(name):
+    parts = name.split('.')
+    module = __import__(parts[0])
+    for part in parts[1:]:
+        module = getattr(module, part)
+    return module
+
