a0aa80c4ed0f376d8dd14d84774c73661363a715
RoseLYi <161707875+RoseLYi@users.noreply.github.com>
2024-11-12 19:51:00 +0800

    Update tools.py

--- feeders/tools.py
+++ feeders/tools.py
@@ -1,234 +1,128 @@
 import random
-import matplotlib.pyplot as plt
 import numpy as np
-import pdb
-
 import torch
 import torch.nn.functional as F
 
-def valid_crop_resize(data_numpy,valid_frame_num,p_interval,window):
-    # input: C,T,V,M
-    C, T, V, M = data_numpy.shape
-    begin = 0
-    end = valid_frame_num
-    valid_size = end - begin
 
-    #crop
-    if len(p_interval) == 1:
-        p = p_interval[0]
-        bias = int((1-p) * valid_size/2)
-        data = data_numpy[:, begin+bias:end-bias, :, :]# center_crop
-        cropped_length = data.shape[1]
+def crop_and_resize(data, valid_frames, interval, window_size):
+    channels, frames, vertices, persons = data.shape
+    start, end = 0, valid_frames
+    total_valid = end - start
+
+    # Crop
+    if len(interval) == 1:
+        p = interval[0]
+        offset = int((1 - p) * total_valid / 2)
+        cropped_data = data[:, start + offset:end - offset, :, :]
     else:
-        p = np.random.rand(1)*(p_interval[1]-p_interval[0])+p_interval[0]
-        cropped_length = np.minimum(np.maximum(int(np.floor(valid_size*p)),64), valid_size)# constraint cropped_length lower bound as 64
-        bias = np.random.randint(0,valid_size-cropped_length+1)
-        data = data_numpy[:, begin+bias:begin+bias+cropped_length, :, :]
-        if data.shape[1] == 0:
-            print(cropped_length, bias, valid_size)
+        p = np.random.uniform(*interval)
+        cropped_len = min(max(int(total_valid * p), 64), total_valid)
+        offset = np.random.randint(0, total_valid - cropped_len + 1)
+        cropped_data = data[:, start + offset:start + offset + cropped_len, :, :]
 
-    # resize
-    data = torch.tensor(data,dtype=torch.float)
-    data = data.permute(0, 2, 3, 1).contiguous().view(C * V * M, cropped_length)
-    data = data[None, None, :, :]
-    data = F.interpolate(data, size=(C * V * M, window), mode='bilinear',align_corners=False).squeeze() # could perform both up sample and down sample
-    data = data.contiguous().view(C, V, M, window).permute(0, 3, 1, 2).contiguous().numpy()
+    # Resize
+    cropped_data = torch.tensor(cropped_data, dtype=torch.float32)
+    cropped_data = cropped_data.permute(0, 2, 3, 1).reshape(-1, cropped_data.shape[1])[None, None, :, :]
+    resized_data = F.interpolate(cropped_data, size=(channels * vertices * persons, window_size),
+                                 mode='bilinear', align_corners=False).squeeze()
+    resized_data = resized_data.reshape(channels, vertices, persons, window_size).permute(0, 3, 1, 2).numpy()
 
+    return resized_data
+
+
+def downsample_data(data, factor, randomize=True):
+    start_idx = np.random.randint(factor) if randomize else 0
+    return data[:, start_idx::factor, :, :]
+
+
+def temporal_slice(data, factor):
+    channels, frames, vertices, persons = data.shape
+    return data.reshape(channels, frames // factor, factor, vertices, persons).transpose(0, 1, 3, 2, 4).reshape(
+        channels, frames // factor, vertices, factor * persons)
+
+
+def subtract_mean(data, mean_val):
+    if mean_val == 0:
+        return
+    channels, frames, vertices, persons = data.shape
+    valid_frames = (data != 0).sum(axis=(3, 2, 0)) > 0
+    start, end = valid_frames.argmax(), len(valid_frames) - valid_frames[::-1].argmax()
+    data[:, :end, :, :] -= mean_val
     return data
 
-def downsample(data_numpy, step, random_sample=True):
-    # input: C,T,V,M
-    begin = np.random.randint(step) if random_sample else 0
-    return data_numpy[:, begin::step, :, :]
+
+def pad_data(data, target_size, randomize=False):
+    channels, frames, vertices, persons = data.shape
+    if frames < target_size:
+        offset = random.randint(0, target_size - frames) if randomize else 0
+        padded_data = np.zeros((channels, target_size, vertices, persons))
+        padded_data[:, offset:offset + frames, :, :] = data
+        return padded_data
+    return data
 
 
-def temporal_slice(data_numpy, step):
-    # input: C,T,V,M
-    C, T, V, M = data_numpy.shape
-    return data_numpy.reshape(C, T / step, step, V, M).transpose(
-        (0, 1, 3, 2, 4)).reshape(C, T / step, V, step * M)
-
-
-def mean_subtractor(data_numpy, mean):
-    # input: C,T,V,M
-    # naive version
-    if mean == 0:
-        return
-    C, T, V, M = data_numpy.shape
-    valid_frame = (data_numpy != 0).sum(axis=3).sum(axis=2).sum(axis=0) > 0
-    begin = valid_frame.argmax()
-    end = len(valid_frame) - valid_frame[::-1].argmax()
-    data_numpy[:, :end, :, :] = data_numpy[:, :end, :, :] - mean
-    return data_numpy
-
-
-def auto_pading(data_numpy, size, random_pad=False):
-    C, T, V, M = data_numpy.shape
-    if T < size:
-        begin = random.randint(0, size - T) if random_pad else 0
-        data_numpy_paded = np.zeros((C, size, V, M))
-        data_numpy_paded[:, begin:begin + T, :, :] = data_numpy
-        return data_numpy_paded
+def random_crop(data, crop_size, pad_if_needed=True):
+    channels, frames, vertices, persons = data.shape
+    if frames == crop_size:
+        return data
+    elif frames < crop_size and pad_if_needed:
+        return pad_data(data, crop_size, randomize=True)
     else:
-        return data_numpy
+        start = random.randint(0, frames - crop_size)
+        return data[:, start:start + crop_size, :, :]
 
 
-def random_choose(data_numpy, size, auto_pad=True):
-    # input: C,T,V,M 随机选择其中一段，不是很合理。因为有0
-    C, T, V, M = data_numpy.shape
-    if T == size:
-        return data_numpy
-    elif T < size:
-        if auto_pad:
-            return auto_pading(data_numpy, size, random_pad=True)
-        else:
-            return data_numpy
-    else:
-        begin = random.randint(0, T - size)
-        return data_numpy[:, begin:begin + size, :, :]
-
-def random_move(data_numpy,
-                angle_candidate=[-10., -5., 0., 5., 10.],
-                scale_candidate=[0.9, 1.0, 1.1],
-                transform_candidate=[-0.2, -0.1, 0.0, 0.1, 0.2],
-                move_time_candidate=[1]):
-    # input: C,T,V,M
-    C, T, V, M = data_numpy.shape
-    move_time = random.choice(move_time_candidate)
-    node = np.arange(0, T, T * 1.0 / move_time).round().astype(int)
-    node = np.append(node, T)
-    num_node = len(node)
-
-    A = np.random.choice(angle_candidate, num_node)
-    S = np.random.choice(scale_candidate, num_node)
-    T_x = np.random.choice(transform_candidate, num_node)
-    T_y = np.random.choice(transform_candidate, num_node)
-
-    a = np.zeros(T)
-    s = np.zeros(T)
-    t_x = np.zeros(T)
-    t_y = np.zeros(T)
-
-    # linspace
-    for i in range(num_node - 1):
-        a[node[i]:node[i + 1]] = np.linspace(
-            A[i], A[i + 1], node[i + 1] - node[i]) * np.pi / 180
-        s[node[i]:node[i + 1]] = np.linspace(S[i], S[i + 1],
-                                             node[i + 1] - node[i])
-        t_x[node[i]:node[i + 1]] = np.linspace(T_x[i], T_x[i + 1],
-                                               node[i + 1] - node[i])
-        t_y[node[i]:node[i + 1]] = np.linspace(T_y[i], T_y[i + 1],
-                                               node[i + 1] - node[i])
-
-    theta = np.array([[np.cos(a) * s, -np.sin(a) * s],
-                      [np.sin(a) * s, np.cos(a) * s]])
-
-    # perform transformation
-    for i_frame in range(T):
-        xy = data_numpy[0:2, i_frame, :, :]
-        new_xy = np.dot(theta[:, :, i_frame], xy.reshape(2, -1))
-        new_xy[0] += t_x[i_frame]
-        new_xy[1] += t_y[i_frame]
-        data_numpy[0:2, i_frame, :, :] = new_xy.reshape(2, V, M)
-
-    return data_numpy
+def apply_random_shift(data):
+    channels, frames, vertices, persons = data.shape
+    shifted_data = np.zeros_like(data)
+    valid_frames = (data != 0).sum(axis=(3, 2, 0)) > 0
+    start, end = valid_frames.argmax(), len(valid_frames) - valid_frames[::-1].argmax()
+    offset = random.randint(0, frames - (end - start))
+    shifted_data[:, offset:offset + (end - start), :, :] = data[:, start:end, :, :]
+    return shifted_data
 
 
-def random_shift(data_numpy):
-    C, T, V, M = data_numpy.shape
-    data_shift = np.zeros(data_numpy.shape)
-    valid_frame = (data_numpy != 0).sum(axis=3).sum(axis=2).sum(axis=0) > 0
-    begin = valid_frame.argmax()
-    end = len(valid_frame) - valid_frame[::-1].argmax()
-
-    size = end - begin
-    bias = random.randint(0, T - size)
-    data_shift[:, bias:bias + size, :, :] = data_numpy[:, begin:end, :, :]
-
-    return data_shift
+def random_rotation(data, theta=0.3):
+    data_tensor = torch.tensor(data)
+    channels, frames, vertices, persons = data_tensor.shape
+    data_tensor = data_tensor.permute(1, 0, 2, 3).reshape(frames, channels, vertices * persons)
+    rotation = torch.rand(3).uniform_(-theta, theta)
+    rotation_matrices = create_rotation_matrix(torch.stack([rotation] * frames, dim=0))
+    rotated_data = torch.matmul(rotation_matrices, data_tensor)
+    return rotated_data.view(frames, channels, vertices, persons).permute(1, 0, 2, 3).numpy()
 
 
-def _rot(rot):
-    """
-    rot: T,3
-    """
-    cos_r, sin_r = rot.cos(), rot.sin()  # T,3
-    zeros = torch.zeros(rot.shape[0], 1)  # T,1
-    ones = torch.ones(rot.shape[0], 1)  # T,1
-
-    r1 = torch.stack((ones, zeros, zeros),dim=-1)  # T,1,3
-    rx2 = torch.stack((zeros, cos_r[:,0:1], sin_r[:,0:1]), dim = -1)  # T,1,3
-    rx3 = torch.stack((zeros, -sin_r[:,0:1], cos_r[:,0:1]), dim = -1)  # T,1,3
-    rx = torch.cat((r1, rx2, rx3), dim = 1)  # T,3,3
-
-    ry1 = torch.stack((cos_r[:,1:2], zeros, -sin_r[:,1:2]), dim =-1)
-    r2 = torch.stack((zeros, ones, zeros),dim=-1)
-    ry3 = torch.stack((sin_r[:,1:2], zeros, cos_r[:,1:2]), dim =-1)
-    ry = torch.cat((ry1, r2, ry3), dim = 1)
-
-    rz1 = torch.stack((cos_r[:,2:3], sin_r[:,2:3], zeros), dim =-1)
-    r3 = torch.stack((zeros, zeros, ones),dim=-1)
-    rz2 = torch.stack((-sin_r[:,2:3], cos_r[:,2:3],zeros), dim =-1)
-    rz = torch.cat((rz1, rz2, r3), dim = 1)
-
-    rot = rz.matmul(ry).matmul(rx)
-    return rot
+def create_rotation_matrix(rotations):
+    cos_vals, sin_vals = rotations.cos(), rotations.sin()
+    zeros, ones = torch.zeros_like(cos_vals[:, 0:1]), torch.ones_like(cos_vals[:, 0:1])
+    rx = torch.stack((ones, zeros, zeros, zeros, cos_vals[:, 0:1], sin_vals[:, 0:1],
+                      zeros, -sin_vals[:, 0:1], cos_vals[:, 0:1]), dim=-1).reshape(-1, 3, 3)
+    ry = torch.stack((cos_vals[:, 1:2], zeros, -sin_vals[:, 1:2], zeros, ones, zeros,
+                      sin_vals[:, 1:2], zeros, cos_vals[:, 1:2]), dim=-1).reshape(-1, 3, 3)
+    rz = torch.stack((cos_vals[:, 2:3], sin_vals[:, 2:3], zeros, -sin_vals[:, 2:3], cos_vals[:, 2:3],
+                      zeros, zeros, zeros, ones), dim=-1).reshape(-1, 3, 3)
+    return rz @ ry @ rx
 
 
-def random_rot(data_numpy, theta=0.3):
-    """
-    data_numpy: C,T,V,M
-    """
-    data_torch = torch.from_numpy(data_numpy)
-    C, T, V, M = data_torch.shape
-    data_torch = data_torch.permute(1, 0, 2, 3).contiguous().view(T, C, V*M)  # T,3,V*M
-    rot = torch.zeros(3).uniform_(-theta, theta)
-    rot = torch.stack([rot, ] * T, dim=0)
-    rot = _rot(rot)  # T,3,3
-    data_torch = torch.matmul(rot, data_torch)
-    data_torch = data_torch.view(T, C, V, M).permute(1, 0, 2, 3).contiguous()
+def match_pose(data):
+    channels, frames, vertices, persons = data.shape
+    assert channels == 3
+    score = data[2, :, :, :].sum(axis=1)
+    rank = (-score[:-1]).argsort(axis=1).reshape(frames - 1, persons)
+    xy1, xy2 = data[:2, :-1, :, :].reshape(2, frames - 1, vertices, persons, 1), \
+               data[:2, 1:, :, :].reshape(2, frames - 1, vertices, 1, persons)
+    distances = ((xy2 - xy1) ** 2).sum(axis=(0, 2))
+    forward_map = np.full((frames, persons), -1, dtype=int)
+    forward_map[0] = np.arange(persons)
+    for m in range(persons):
+        match = (rank == m)
+        best_match = distances[match].argmin(axis=1)
+        for t in range(frames - 1):
+            distances[t, :, best_match[t]] = np.inf
+        forward_map[1:][match] = best_match
+    forward_map[1:] = forward_map[1:][forward_map[:-1]]
+    matched_data = np.zeros_like(data)
+    for t in range(frames):
+        matched_data[:, t, :, :] = data[:, t, :, forward_map[t]]
+    return matched_data[:, :, :, (-matched_data[2].sum(axis=1).sum(axis=0)).argsort()]
 
-    return data_torch
-
-def openpose_match(data_numpy):
-    C, T, V, M = data_numpy.shape
-    assert (C == 3)
-    score = data_numpy[2, :, :, :].sum(axis=1)
-    # the rank of body confidence in each frame (shape: T-1, M)
-    rank = (-score[0:T - 1]).argsort(axis=1).reshape(T - 1, M)
-
-    # data of frame 1
-    xy1 = data_numpy[0:2, 0:T - 1, :, :].reshape(2, T - 1, V, M, 1)
-    # data of frame 2
-    xy2 = data_numpy[0:2, 1:T, :, :].reshape(2, T - 1, V, 1, M)
-    # square of distance between frame 1&2 (shape: T-1, M, M)
-    distance = ((xy2 - xy1) ** 2).sum(axis=2).sum(axis=0)
-
-    # match pose
-    forward_map = np.zeros((T, M), dtype=int) - 1
-    forward_map[0] = range(M)
-    for m in range(M):
-        choose = (rank == m)
-        forward = distance[choose].argmin(axis=1)
-        for t in range(T - 1):
-            distance[t, :, forward[t]] = np.inf
-        forward_map[1:][choose] = forward
-    assert (np.all(forward_map >= 0))
-
-    # string data
-    for t in range(T - 1):
-        forward_map[t + 1] = forward_map[t + 1][forward_map[t]]
-
-    # generate data
-    new_data_numpy = np.zeros(data_numpy.shape)
-    for t in range(T):
-        new_data_numpy[:, t, :, :] = data_numpy[:, t, :, forward_map[
-                                                             t]].transpose(1, 2, 0)
-    data_numpy = new_data_numpy
-
-    # score sort
-    trace_score = data_numpy[2, :, :, :].sum(axis=1).sum(axis=0)
-    rank = (-trace_score).argsort()
-    data_numpy = data_numpy[:, :, :, rank]
-
-    return data_numpy
